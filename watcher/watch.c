17db8e4c9d7fc13c3baefa41977e28912fb6ad04
abrachet <alexbrachetmialot@gmail.com>
2019-09-26 01:02:05 -0400

    watcher runs runfile

--- watcher/watch.c
+++ watcher/watch.c
@@ -1,7 +1,13 @@
+/**
+ * Alex Brachet (abrachet@cs.purdue.edu) 
+ */
 
-#include <fcntl.h> // for oflags
-#include <sys/inotify.h> // for IN_CLOEXEC
+#include <sys/inotify.h> // for IN_CLOEXEC and sturct inotify_event
+#include <signal.h> // for SIGKILL
 #include <stdint.h>
+
+typedef uint64_t size_t;
+typedef int64_t ssize_t;
 
 extern int syscall(int, ...);
 
@@ -9,34 +15,45 @@ static inline ssize_t read(int fd, void *buf, size_t len) {
     return syscall(0, fd, buf, len);
 }
 
-static inline ssize_t write(int fd, const void *buf, size_t len) {
-    return syscall(1, fd, buf, len);
+static pid_t fork(void) {
+    return syscall(57);
 }
 
-// #define this because it conflicts with open in fcntl.h
-#define open(path, oflag) syscall(2, path, oflag)
+static int execve(const char *filename, char *const argv[], char *const envp[]) {
+    return syscall(59, filename, argv, envp);
+}
 
-static inline int _exit(int status) {
+static inline void _exit(int status) {
     syscall(60, status);
 }
 
+#define kill(pid, sig) syscall(62, pid, sig)
 #define inotify_init1(flags) syscall(294, flags)
 #define inotify_add_watch(fd, path, mask) syscall(254, fd, path, mask)
 
+static char buf[1024];
+void runfile(char *const argv[]) {
+    pid_t pid = fork();
+    
+    if (pid < 0)
+        goto die;
+ 
+    if (pid > 0)
+       return;
+
+    execve(*argv, argv, 0);
+die:
+    kill(0, SIGKILL);
+}
+
 int main(int argc, const char **argv) {
-    // argv[1] is file to watch and then execute
-    int watchfile = open(argv[1], O_RDONLY);
     int inotifyfd = inotify_init1(IN_CLOEXEC);
-    int wd = inotify_add_watch(inotifyfd, argv[1], IN_ALL_EVENTS);
-    if (wd < 0)
+    if(inotify_add_watch(inotifyfd, argv[1], IN_MODIFY) < 0)
         _exit(127);
 
-
-    char buf[1024];
     for (;;) {
-        ssize_t amount_read = read(wd, buf, 1024);
-        struct inotify_event* event = buf;
-        write(1, "event!\n", 8);
+        ssize_t amount_read = read(inotifyfd, buf, 1024);
+        struct inotify_event* event = (struct inotify_event*) buf;
+        runfile((char *const *)argv + 1); 
     }
-
 }
