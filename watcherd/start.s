2ac17bbd255458416c9aaba526ca97384ecebd12
abrachet <alexbrachetmialot@gmail.com>
2019-09-26 01:48:39 -0400

    remove useless cfi directives

--- watcherd/start.s
+++ watcherd/start.s
@@ -5,19 +5,13 @@
 .align 4
 .global _start
 _start:
-    .cfi_startproc
-    .cfi_undefined rip
     xorl %ebp, %ebp
 
     mov %rdx, %r9
     popq %rdi
 
     mov %rsp, %rsi
-    //and $~15, %rsp
-    //pushq %rax
-    //pushq %rsp
 
     call main
     // program never exits normally it will always be killed, don't bother
     // calling sys_exit
-    .cfi_endproc
